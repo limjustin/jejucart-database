430881f50cc01e06dd9dfe800137b96a1a923d31
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-23 09:40:26 +0900

    Update init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -6,11 +6,11 @@ FLUSH PRIVILEGES;
 
 USE `krampoline`;
 
-DROP TABLE IF EXISTS `sample_data`;
-CREATE TABLE `sample_data` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `detail` varchar(100) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- DROP TABLE IF EXISTS `sample_data`;
+-- CREATE TABLE `sample_data` (
+--     `id` int(11) NOT NULL AUTO_INCREMENT,
+--     `detail` varchar(100) NOT NULL,
+--     PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
+-- INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
